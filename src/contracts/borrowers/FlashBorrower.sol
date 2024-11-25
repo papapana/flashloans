68ca486c70b0cc3c6a59a75fdf38fdae1c1521d1
Vassilis Papapanagiotou <bpapapana@gmail.com>
2024-11-25 22:03:42 +0100

    Add comments

--- src/contracts/borrowers/FlashBorrower.sol
+++ src/contracts/borrowers/FlashBorrower.sol
@@ -20,6 +20,15 @@ abstract contract FlashBorrower is IERC3156FlashBorrower {
         OWNER = msg.sender;
     }
 
+    /**
+     * @notice Initiates a flash loan borrowing process
+     * @dev This function sets up and executes a flash loan
+     * @param _lender The address of the flash loan lender
+     * @param token The address of the token to borrow
+     * @param amount The amount of tokens to borrow
+     * @param data Additional data to pass to the flash loan
+     * @custom:throws BorrowingFailed if the flash loan fails to execute
+     */
     function borrow(IERC3156FlashLender _lender, address token, uint256 amount, bytes calldata data) external {
         lender = _lender;
         // We have to take into account existing loans
@@ -31,11 +40,30 @@ abstract contract FlashBorrower is IERC3156FlashBorrower {
         if (!borrowed) revert BorrowingFailed();
     }
 
+    /**
+     * @notice Performs actions with the borrowed funds
+     * @dev This function should be overridden by inheriting contracts to define specific actions
+     * @param initiator The address that initiated the flash loan
+     * @param token The address of the borrowed token
+     * @param amount The amount of tokens borrowed
+     * @param fee The fee for the flash loan
+     * @param data Additional data passed from the flash loan
+     * @return A boolean indicating whether the actions were successful
+     */
     function act(address initiator, address token, uint256 amount, uint256 fee, bytes calldata data)
         internal
         virtual
         returns (bool);
 
+    /**
+     * @dev Receive a flash loan.
+     * @param initiator The initiator of the loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @param fee The additional amount of tokens to repay.
+     * @param data Arbitrary data structure, intended to contain user-defined parameters.
+     * @return The keccak256 hash of "ERC3156FlashBorrower.onFlashLoan"
+     */
     function onFlashLoan(address initiator, address token, uint256 amount, uint256 fee, bytes calldata data)
         external
         returns (bytes32)
